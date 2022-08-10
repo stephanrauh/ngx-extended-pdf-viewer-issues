d11de5a2d3724be89dbf5202b2a45f8e0824245c
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2022-08-10 21:54:46 +0200

    stephanrauh/ngx-extended-pdf-viewer#1485 quick workaround

--- issue1485/src/app/example-pdf-viewer/example-pdf-viewer.component.ts
+++ issue1485/src/app/example-pdf-viewer/example-pdf-viewer.component.ts
@@ -8,20 +8,10 @@ import { NgxExtendedPdfViewerService, pdfDefaultOptions } from 'ngx-extended-pdf
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ExamplePdfViewerComponent {
-  /** In most cases, you don't need the NgxExtendedPdfViewerService. It allows you
-   *  to use the "find" api, to extract text and images from a PDF file,
-   *  to print programmatically, and to show or hide layers by a method call.
-  */
-  constructor(private pdfService: NgxExtendedPdfViewerService) {
-    /* More likely than not you don't need to tweak the pdfDefaultOptions.
-       They are a collecton of less frequently used options.
-       To illustrate how they're used, here are two example settings: */
-    pdfDefaultOptions.doubleTapZoomFactor = '150%'; // The default value is '200%'
-    pdfDefaultOptions.maxCanvasPixels = 4096 * 4096 * 5; // The default value is 4096 * 4096 pixels,
-    // but most devices support much higher resolutions.
-    // Increasing this setting allows your users to use higher zoom factors,
-    // trading image quality for performance.
 
-    
-    }
+  constructor(private pdfService: NgxExtendedPdfViewerService) {
+    // TODO the PDF viewer should work without the next line - it's merely a workaround:
+    pdfDefaultOptions.cMapUrl= () => `/assets/cmaps/`;
+
+  }
 }
